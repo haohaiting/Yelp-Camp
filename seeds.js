3257fe5a403a7b6bfed15ec69b85f6447487d001
Haiting Hao <haohaiting@gmail.com>
2020-01-14 02:26:51 -0500

    initialize database

--- seeds.js
+++ seeds.js
@@ -1,27 +1,27 @@
 var mongoose = require("mongoose"),
     Campground = require("./models/campground"),
-    Comment = require("./models/comment")
+    Comment = require("./models/comment");
 
 var data = [
     {
         name: "Stone",
         image: "https://images.unsplash.com/photo-1471115853179-bb1d604434e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60",
-        description: "Definately an awesome place!!!"
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     },
     {
         name: "Brook",
         image: "https://images.unsplash.com/photo-1500581276021-a4bbcd0050c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60",
-        description: "I've never been there before. But I fell in love with the image..."
+        description: "I've never been there before. But I fell in love with the image...Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     },
     {
         name: "Beach Park",
         image: "https://images.unsplash.com/photo-1475483768296-6163e08872a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-        description: "Camping is the best. Beach Park is the beat place for camping in summer!!"
+        description: "Camping is the best. Beach Park is the beat place for camping in summer!! Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     }
-]
+];
 
 function seedDB() {
-    Campground.deleteMany({}, (err) => {
+    Campground.deleteMany({}, function(err) {
         if (err) {
             console.log(err);
         } else {
@@ -29,7 +29,7 @@ function seedDB() {
             console.log("Removed all campgrounds!");
             // add a few campgrounds
             data.forEach(function (seed) {
-                Campground.create(seed, (err, campground) => {
+                Campground.create(seed, function(err, campground) {
                     if (err) {
                         console.log(err);
                     } else {
@@ -38,7 +38,7 @@ function seedDB() {
                         Comment.create({
                             text: "I went this place last year. It's awesome! But I wish there's Wi-Fi...",
                             author: "No one"
-                        }, (err, comment) => {
+                        }, function (err, comment) {
                             if(err){
                                 console.log(err);
                             } else {
@@ -46,12 +46,12 @@ function seedDB() {
                                 campground.save();
                                 console.log("Created a new comment!");
                             }
-                        })
+                        });
                     }
-                })
-            })
+                });
+            });
         }
-    })
+    });
 }
 
 module.exports = seedDB;
